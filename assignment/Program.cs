dba3e096258a19d06b1eafad680434bf81e4c61e
Adeleye Emmanuel <eadeleye45@gmail.com>
2021-07-12 19:20:44 +0100

    Assignment

---
+++ assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welome Student \nplease indiate below \nFull-time or part-time");
+            string student = Convert.ToString(Console.ReadLine());
+            {
+                if (student == "full-time")
+                {
+                    Console.WriteLine("course of study?");
+                    string answer = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine(answer + " it is a good course");
+                }
+                else if (student == "part-time")
+                {
+                    
+                
+                    {
+                        Console.WriteLine("how many credit are you taken?");
+                        int credit = Convert.ToInt32(Console.ReadLine());
+                         if (credit > 6)
+                         System.Console.WriteLine("to much unit");
+                         else
+                         {
+                            System.Console.WriteLine("proceed to screen");
+                         }
+                   }
+                }
+                else
+                {
+                    System.Console.WriteLine(" ERROR \ncrosscheck imput");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ new assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace new_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("solve the equation below \nax + by = e \ncx + dy = f");
+            System.Console.WriteLine("a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("c");
+            int c = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("d");
+            int d = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("e");
+            int e = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("f");
+            int f = Convert.ToInt32(Console.ReadLine());
+
+            double g = e*d - f*b;
+            double h = a*d - b*c;
+
+            double i = a*f - e*c;
+            double j = a*d - b*c;
+
+            double x = g/h;
+            double y = i/h;
+            if (h == 0)
+            {
+                System.Console.WriteLine("the equation has no solution");
+            }
+            else
+              {
+               System.Console.WriteLine("x is " + x);
+               System.Console.WriteLine("y is " + y);
+              }
+            
+        }
+    }
+}
